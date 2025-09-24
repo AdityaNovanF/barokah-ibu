562e1dbb6315ec6d50da2c210b2e57d7d7f1e343
AdityaNovanF <aditcarlytos61199@gmail.com>
2025-09-24 15:59:47 +0700

    Update header.php

--- wp-content/themes/barokah-printing/header.php
+++ wp-content/themes/barokah-printing/header.php
@@ -115,7 +115,7 @@
                         </div>
                     <?php else : ?>
                         <div class="flex flex-col">
-                            <a href="<?php echo home_url(); ?>" class="text-3xl font-bold text-blue-600 hover:text-blue-700 transition-colors duration-200 leading-tight">
+                            <a href="<?php echo home_url(); ?>" class="text-2xl font-bold text-blue-600 hover:text-blue-700 transition-colors duration-200 leading-tight">
                                 BAROKAH IBU
                             </a>
                             <span class="text-gray-700 italic text-sm tracking-widest">D i g i t a l  p r i n t i n g</span>
